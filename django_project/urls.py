96efe04e89eddb7f58be3fb629245b1895a07047
hohenstaufen <hydroxide.lithium@gmail.com>
2024-04-11 21:15:27 +0400

    middle stage, app doesn't work yet

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("register/", views.register),
+    path("login/", views.login),
+    path("refresh/", views.refresh),
+    path("logout/", views.logout),
 ]
---
+++ django_project/views.py
@@ -0,0 +1,28 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view('POST')
+def register(request):
+    return Response({})
+
+@api_view('POST')
+def login(request):
+    return Response({})
+
+@api_view('POST')
+def refresh(request):
+    return Response({})
+
+@api_view('POST')
+def logout(request):
+    return Response({})
+'''
+@api_view('GET')
+def me(request):
+    return Response({})
+
+@api_view('PUT')
+def me(request):
+    return Response({})
+
+'''
