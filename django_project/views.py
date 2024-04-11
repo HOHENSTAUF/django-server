c44e078b60557eae6fac1dfe78330cf21d05320e
hohenstaufen <hydroxide.lithium@gmail.com>
2024-04-11 22:52:10 +0400

    added standart tokens and started first quiery, error

--- django_project/views.py
+++ django_project/views.py
@@ -1,8 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .serializers import UserSerializer
+from rest_framework import status
+#from pyjwt import token
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
 @api_view(['POST'])
 def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        token = Token.objects.create(user=user)
+        return Response({"token": token.key, "user": serializer.data})
     return Response({})
 
 @api_view(['POST'])
@@ -17,7 +29,7 @@ def refresh(request):
 def logout(request):
     return Response({})
 
-@api_view('GET')
+@api_view(['GET'])
 def me(request):
     return Response({})
 
