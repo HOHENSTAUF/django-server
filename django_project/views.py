ffd47a293897bf8270d8ccf342bb93d261bd6452
hohenstaufen <hydroxide.lithium@gmail.com>
2024-04-11 21:20:45 +0400

    app works again, now I will code the features

--- django_project/views.py
+++ django_project/views.py
@@ -1,19 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view('POST')
+@api_view(['POST'])
 def register(request):
     return Response({})
 
-@api_view('POST')
+@api_view(['POST'])
 def login(request):
     return Response({})
 
-@api_view('POST')
+@api_view(['POST'])
 def refresh(request):
     return Response({})
 
-@api_view('POST')
+@api_view(['POST'])
 def logout(request):
     return Response({})
 '''
