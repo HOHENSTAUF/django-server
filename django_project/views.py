9875be47d3deceaf5d025c87beca56156c41c961
hohenstaufen <hydroxide.lithium@gmail.com>
2024-04-12 15:52:55 +0400

    register is working

--- django_project/views.py
+++ django_project/views.py
@@ -5,11 +5,10 @@ from rest_framework.response import Response
 from .serializers import UserSerializer
 from rest_framework import status
 #from pyjwt import token
-#from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
 @api_view(['POST'])
-
 def register(request):
     
     serializer = UserSerializer(data=request.data)
@@ -21,7 +20,7 @@ def register(request):
         token = Token.objects.create(user=user)
         return Response({"token": token.key, "user": serializer.data})
     
-    return Response(serializer.errors, status.HTTP_4)
+    return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def login(request):
