fd6f04adbd464a1b4046ad08fcfa616c42765942
hohenstaufen <hydroxide.lithium@gmail.com>
2024-04-12 16:50:45 +0400

    login and register pre-built

--- django_project/views.py
+++ django_project/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 #from pyjwt import token
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 @api_view(['POST'])
 def register(request):
@@ -24,7 +25,12 @@ def register(request):
 
 @api_view(['POST'])
 def login(request):
-    return Response({})
+    user = get_object_or_404(User, email=request.data['email'])
+    if not user.check_password(request.data['password']):
+        return Response({"detail": "not found"}, status = status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)    #required different access token
+    serializer = UserSerializer(instance=user)
+    return Response({"token": token.key, "user": serializer.data})
 
 @api_view(['POST'])
 def refresh(request):
